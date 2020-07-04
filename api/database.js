fb8fb5af16b75c07fc41d6c078f60ad08d457f74
Romeo-Peter <romeomeshack34@gmail.com>
2020-07-04 21:47:58 +0100

    Modified

--- api/database.js
+++ api/database.js
@@ -1,31 +1,31 @@
 const config = require("./config");
 const knex = require("knex")(config);
 
-
 exports.createImage = (url) => {
- return knex("images").insert({imageurl: url})
-  .then(() => console.log("Data inserted into table"))
-  .catch(err => console.log(err))
+  return knex("images")
+    .insert({ imageurl: url })
+    .then(() => console.log("Data inserted into table"))
+    .catch((err) => console.log(err));
   // .finally(() => knex.destroy())
 };
 
-/*exports.deleteImage = (id) => {
-  knex("images").where(id).del()
-  .then(() => console.log("Url deleted from table"))
-  .catch(err => {
-    console.log(err);
-    throw err;
-  })
-  .finally(() => knex.destroy());
-}
+exports.deleteImage = (id) => {
+  return knex("images")
+    .where({ id: id })
+    .del()
+    .then(() => console.log("Url deleted from table"))
+    .catch((err) => console.log(err));
+  // .finally(() => knex.destroy());
+};
 
 exports.listImages = () => {
-  knex.select("id", "url").from("images")
-  .then(() => console.log("Images selected"))
-  .catch(err => {
-    console.log(err);
-    throw err;
-  })
-  .finally(() => knex.destroy());
-}*/
-
+  return knex
+    .from("images")
+    .select("*")
+    .then((rows) => {
+      console.log("Images selected to be listed");
+      return rows;
+    })
+    .catch((err) => console.log(err));
+  // .finally(() => knex.destroy());
+};
