9720754054740e576ce0c4ebda3477e9d20fd738
Romeo-Peter <romeomeshack34@gmail.com>
2020-07-04 21:55:36 +0100

    Popup

--- web/src/components/Popup.js
+++ web/src/components/Popup.js
@@ -1,24 +1,26 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import "./Images.css";
 
-
 class App extends Component {
+  // Close popup and delete image data
+  handleOnclick = () => {
+    this.props.closePopup();
+    const imageId = this.props.popImageId;
+    this.props.deleteImage(imageId);
+  };
+
   render() {
     return (
-
       <div className="popupParent">
         <div className="popupImage">
-            <button
-              className="imageClosingButton"
-              onClick={this.props.closePopup}
-              >
-                X
-              </button>
-            <img src={this.props.popImageUrl}/>
+          <button className="imageClosingButton" onClick={this.handleOnclick}>
+            X
+          </button>
+          <img src={this.props.popImageUrl} id={this.props.popImageId} alt="" />
         </div>
       </div>
-    )
+    );
   }
 }
 
