13e4d50e170a205346567f5d23f1a565daa1bc6b
Romeo-Peter <romeomeshack34@gmail.com>
2020-07-04 21:51:11 +0100

    Modified

--- web/src/ajax.js
+++ web/src/ajax.js
@@ -6,20 +6,41 @@ export const createImage = (url) => {
     url: "http://localhost:8080/images",
     headers: {
       "Content-Type": "application/json",
-      "credentials": "include"
+      credentials: "include",
     },
-    data: {url: url}
-  }).then(res => console.log(res.data.response));
+    data: { url: url },
+  })
+    .then((res) => console.log(res.data.storeResponse))
+    .catch((err) => console.log(err));
 
-  console.log("Sent url to server")
+  console.log("Sent url to server");
 };
 
-/*export const deleteImage = (id) => {
-  axios.delete(`http://localhost:8080/images/${id}`);
-  console.log(id)
+export const deleteImage = (id) => {
+  console.log("Image id sent to server");
+
+  console.log(id);
+  return axios({
+    method: "DELETE",
+    url: `http://localhost:8080/images/${id}`,
+    headers: {
+      "Content-Type": "application/json",
+      credentials: "include",
+    },
+  })
+    .then((res) => console.log(res.data.deleteResponse))
+    .catch((err) => console.log(err));
 };
 
-export const listImages = async () => {
-  const result = await axios.get("http://localhost:8080/images");
-  return result.data;
-};*/
+export const listImages = () => {
+  return axios({
+    method: "GET",
+    url: "http://localhost:8080/images",
+    headers: {
+      "Content-Type": "application/json",
+      credentials: "include",
+    },
+  })
+    .then((res) => console.log(res.data.imagesResponse))
+    .catch((err) => console.log(err));
+};
